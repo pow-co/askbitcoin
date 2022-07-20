afc15d96efaf75c4bfc7b30d5f88587177a7af9b
Guillaume De Martino <guillaumedemartino@gmail.com>
2022-07-20 19:19:16 +0200

    adds query params to useAPI

--- web-ui/src/pages/questions/index.js
+++ web-ui/src/pages/questions/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 // material-ui
 import { Typography, Grid, CardContent, Stack, Rating } from '@mui/material';
 
@@ -23,11 +24,10 @@ import { useEvents } from 'hooks/useEvents';
 import FormControlSelect from 'components/ui-component/extended/Form/FormControlSelect';
 
 import { useRouter } from 'next/router';
-import axios from 'utils/axios'
-
+import axios from 'utils/axios';
 
 const QuestionPage = () => {
-  window.postQuestion = postQuestion;
+  const [queryParams, setQueryParams] = useState('');
   const { user, wallet, isLoggedIn } = useAuth();
 
   const router = useRouter();
@@ -35,86 +35,82 @@ const QuestionPage = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   async function postQuestion(content) {
-
-    if(!isLoggedIn){
+    if (!isLoggedIn) {
       enqueueSnackbar('Please, Log In', {
         anchorOrigin: {
           vertical: 'top',
-          horizontal:'center'
+          horizontal: 'center'
         },
-        variant:'error'
-      })
-      return
+        variant: 'error'
+      });
+      return;
     }
 
     try {
+      console.log('postQuestion', content);
 
-        console.log('postQuestion', content)
+      enqueueSnackbar(`Posting Question: ${content}`, {
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'center'
+        },
+        variant: 'info'
+      });
 
-        enqueueSnackbar(`Posting Question: ${content}`, {
-          anchorOrigin: {
-            vertical: 'top',
-            horizontal: 'center'
-          },
-          variant: 'info'
-        })
-
-        switch (wallet){
-          case "relayx":
-            let result = await relayone.send({
-              opReturn: ['onchain', '1HWaEAD5TXC2fWHDiua9Vue3Mf8V1ZmakN', 'question', JSON.stringify({
+      switch (wallet) {
+        case 'relayx':
+          let result = await relayone.send({
+            opReturn: [
+              'onchain',
+              '1HWaEAD5TXC2fWHDiua9Vue3Mf8V1ZmakN',
+              'question',
+              JSON.stringify({
                 content
-              })],
-              currency: 'USD',
-              amount: 0.01,
-              to: '1HWaEAD5TXC2fWHDiua9Vue3Mf8V1ZmakN'
-            })
-            let {
-              amount,
-              currency,
-              identity,
-              paymail,
-              rawTx,
-              satoshis,
-              txid
-            } = result
-        
-            enqueueSnackbar(`Question Posted by ${paymail}`, {
-              anchorOrigin: {
-                vertical: 'top',
-                horizontal: 'center'
-              },
-              variant: 'success'
-            })
-        
-            let { data: postTransactionResponse } = await axios.post('/api/v1/transactions', {
-              transaction: rawTx
-            })
-    
-            console.log('postTransactionResponse', postTransactionResponse);
-    
-            router.push(`/questions/${txid}`)
-            break;
-          case "twetch":
-            //TODO
-            break;
-          case "handcash":
-            //TODO
-            break;
-          default:
-            console.error("No wallet selected")
-            return
-        }
-      } catch(error) {
-        enqueueSnackbar(`Error Posting Question: ${error.message}`, {
-          anchorOrigin: {
-            vertical: 'top',
-            horizontal: 'center'
-          },
-          variant: 'error'
-        })
+              })
+            ],
+            currency: 'USD',
+            amount: 0.01,
+            to: '1HWaEAD5TXC2fWHDiua9Vue3Mf8V1ZmakN'
+          });
+          let { amount, currency, identity, paymail, rawTx, satoshis, txid } = result;
+
+          enqueueSnackbar(`Question Posted by ${paymail}`, {
+            anchorOrigin: {
+              vertical: 'top',
+              horizontal: 'center'
+            },
+            variant: 'success'
+          });
+
+          let { data: postTransactionResponse } = await axios.post('/api/v1/transactions', {
+            transaction: rawTx
+          });
+
+          console.log('postTransactionResponse', postTransactionResponse);
+
+          router.push(`/questions/${txid}`);
+          break;
+        case 'twetch':
+          //TODO
+          break;
+        case 'handcash':
+          //TODO
+          break;
+        default:
+          console.error('No wallet selected');
+          return;
       }
+    } catch (error) {
+      enqueueSnackbar(`Error Posting Question: ${error.message}`, {
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'center'
+        },
+        variant: 'error'
+      });
+    }
   }
+  window.postQuestion = postQuestion;
 
   function onQuestion(question) {
     console.log('on question', question);
@@ -125,11 +121,11 @@ const QuestionPage = () => {
 
   window.events = events;
 
-  let { data, error, refresh, loading } = useAPI('/questions');
+  let { data, error, refresh, loading } = useAPI(`/questions`, queryParams);
 
   console.log({ data, error, refresh, loading });
 
-  if (error) {
+  if (!loading && (error || data === undefined)) {
     console.log('ERROR', error);
     return <p>Error</p>;
   }
@@ -141,6 +137,10 @@ const QuestionPage = () => {
       </p>
     );
   }
+
+  const onChangeFilter = (filter) => {
+    setQueryParams(filter.query);
+  };
 
   const { questions } = data;
 
@@ -154,7 +154,7 @@ const QuestionPage = () => {
           </Typography>
         </Grid>
         <Grid item xs={6}>
-          <FormControlSelect />
+          <FormControlSelect handleFilter={onChangeFilter} />
         </Grid>
       </Grid>
       <Stack direction="column" justifyContent="flex-end">
