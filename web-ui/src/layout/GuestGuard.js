53d993ad081b4f1e5a0972e38b8c6477770721bd
Guillaume De Martino <guillaumedemartino@gmail.com>
2022-08-04 21:16:19 +0200

    add locale selection on Login page

--- web-ui/src/layout/GuestGuard.js
+++ web-ui/src/layout/GuestGuard.js
@@ -1,22 +1,45 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
+
+//material-ui
+import { useTheme } from '@mui/styles';
+import { Toolbar, AppBar, CssBaseline } from '@mui/material';
 
 // project imports
 import Customization from './Customization';
 import GuestGuard from 'utils/route-guard/GuestGuard';
 import NavMotion from './NavMotion';
+import Header from './MainLayout/Header/Login';
 
 // ==============================|| MINIMAL LAYOUT ||============================== //
 
-const GuestGuardLayout = ({ children }) => (
-  <NavMotion>
-    <GuestGuard>
-      <>
-        {children}
-        <Customization />
-      </>
-    </GuestGuard>
-  </NavMotion>
-);
+const GuestGuardLayout = ({ children }) => {
+  const theme = useTheme;
+  const header = useMemo(
+    () => (
+      <Toolbar>
+        <Header />
+      </Toolbar>
+    ),
+    []
+  );
+
+  return (
+    <NavMotion>
+      <GuestGuard>
+        <>
+          <CssBaseline />
+
+          <AppBar enableColorOnDark position="fixed" color="inherit" elevation={0} sx={{}}>
+            {header}
+          </AppBar>
+          {children}
+          <Customization />
+        </>
+      </GuestGuard>
+    </NavMotion>
+  );
+};
 
 GuestGuardLayout.propTypes = {
   children: PropTypes.node
---
+++ web-ui/src/layout/MainLayout/Header/Login.js
@@ -0,0 +1,36 @@
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Avatar, Box } from '@mui/material';
+
+// project imports
+import LogoSection from '../LogoSection';
+import SearchSection from './SearchSection';
+import MobileSection from './MobileSection';
+import ProfileSection from './ProfileSection';
+import LocalizationSection from './LocalizationSection';
+import NotificationSection from './NotificationSection';
+import { useDispatch, useSelector } from 'store';
+import { openDrawer } from 'store/slices/menu';
+
+// assets
+import { IconMenu2 } from '@tabler/icons';
+
+// ==============================|| MAIN NAVBAR / HEADER ||============================== //
+
+const Header = () => {
+  const theme = useTheme();
+
+  const dispatch = useDispatch();
+  const { drawerOpen } = useSelector((state) => state.menu);
+
+  return (
+    <>
+      {/* live customization & localization */}
+      <Box sx={{ display: { xs: 'block' } }}>
+        <LocalizationSection />
+      </Box>
+    </>
+  );
+};
+
+export default Header;
