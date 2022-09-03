0f3ea721560e9ff22530b0a832ae7d382fff1812
Zyler <508282+zylerdj@users.noreply.github.com>
2022-09-03 04:26:57 +0200

    fix(prod-api): use production API

--- web-ui/src/hooks/useAPI.js
+++ web-ui/src/hooks/useAPI.js
@@ -1,13 +1,12 @@
 import useSWR from 'swr';
 
 //export const BASE = `/api/v1`;
-//export const BASE = `https://askbitcoin.ai/api/v1`;
-export const BASE = `http://localhost:5200/api/v1`;
+export const BASE = `https://askbitcoin.ai/api/v1`;
 
 import axios from '../utils/axios';
 
 //const axiosInstance = axios.create({ baseURL: process.env.HOST_API_KEY || '' });
-const axiosInstance = axios.create({ baseURL: 'http://localhost:5200' });
+const axiosInstance = axios.create({ baseURL: 'https://askbitcoin.ai' });
 
 axiosInstance.interceptors.response.use(
   (response) => response,
