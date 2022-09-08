d0acef00a3223ec61ce66dcf74939a2a36a9fc2f
Zyler <508282+zylerdj@users.noreply.github.com>
2022-09-08 13:41:25 +0200

    perf(loki-logging): optionally send logs to loki-grafana server

--- src/log.ts
+++ src/log.ts
@@ -1,13 +1,52 @@
 
-const winston = require('winston');
+import * as winston from 'winston';
+
+import config from './config'
+
+const transports = [
+  new winston.transports.Console({
+    format: winston.format.json()
+  })
+]
+
+if (config.get('loki_enabled') && config.get('loki_host')) {
+
+  console.log('loki enabled')
+
+  const LokiTransport = require("winston-loki");
+
+  const lokiConfig = {
+    format: winston.format.json(),
+    host: config.get('loki_host'),
+    json: true,
+    batching: false,
+    labels: { app: config.get('loki_label_app') }
+  }
+
+  if (config.get('loki_basic_auth')) {
+
+    lokiConfig['basicAuth'] = config.get('loki_basic_auth')
+  }
+
+  transports.push(
+    new LokiTransport(lokiConfig)
+  )
+
+}
 
 const log = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: '' },
-  transports: [
-    new winston.transports.Console(),
-  ],
+  transports,
+  format: winston.format.json()
 });
 
+if (config.get('loki_host')) {
+
+  log.info('loki.enabled')
+
+}
+
+export default log
+
 export { log }
+
